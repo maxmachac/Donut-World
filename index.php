c1a305afa0d438ffc57ea2b7c81f3473c139ae3a
maxmachac <155898864+maxmachac@users.noreply.github.com>
2026-03-17 19:11:31 +0100

    Delete index.php file content
    Removed the entire HTML structure and PHP code.

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<header>
-    <h1>Donut World</h1>
-</header>
-<img src="https://learn.alg.academy/uploads/2023/02/donut.png">
-  <?php
-    echo "Donut World!";
-  ?>
-    
-</body>
-</html>
+
